451695d1298ed3f2f0c74dfe26af0a47b7f6e21b
Alex <friedmanalexg@gmail.com>
2022-06-09 13:20:54 -0400

    commit tree 4e8f7b4071aaffc5c25afd19164df4ca1cb1a192

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solutions in this file
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+const cards = []
+const friends = ["Guadalupe","Ollie", "Aki"] ;
+const event1 = "surprise"
+
+function writeCards(friends, event) {
+    for (let i = 0; i < friends.length; i++) {
+        cards.push(`Thank you, ${friends[i]}, for the wonderful ${event} gift!`);
+        }
+    return cards        
+}
+
+ //yeah, i really wish the ACTUAL expectation for the code above was CLEARLY EXPLAINED in the assignment instructions
+
+ 
+function countDown(i) {
+    while (i >= 0) {
+        i--;
+        console.log(i+1);
+    }
+ }
+
+ 
